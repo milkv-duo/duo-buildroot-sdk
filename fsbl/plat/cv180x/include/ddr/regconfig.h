b5490999b7d04a00651d3ef5eb475358d84dc477
forum_service <forum_service@sophgo.com>
2023-08-28 19:32:42 +0800

    fsbl: version release v4.1.0.3
    6d7905a [180x] sync ddr changes to cvitek/fsbl master branch
    
    Change-Id: I33696f4f6221274b1a501a3c4f22884f89f96bce

--- fsbl/plat/cv180x/include/ddr/regconfig.h
+++ fsbl/plat/cv180x/include/ddr/regconfig.h
@@ -21,8 +21,14 @@ struct regpatch {
 // #define DDR_CFG_TYPE_DDR2	2
 // #define DDR_CFG_TYPE_DDR3_AUTO	3
 // #define DDR_CFG_TYPE_DDR2_AUTO	4
-
+#ifdef DDR2_3
+extern struct regpatch ddr3_1866_patch_regs[];
+extern uint32_t ddr3_1866_patch_regs_count;
+extern struct regpatch ddr2_1333_patch_regs[];
+extern uint32_t ddr2_1333_patch_regs_count;
+#else
 extern struct regpatch ddr_patch_regs[];
 extern uint32_t ddr_patch_regs_count;
+#endif
 
 #endif /* __REG_CFG_H__ */
