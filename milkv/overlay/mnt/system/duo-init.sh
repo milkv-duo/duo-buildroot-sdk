bb552e56ddf6bf1e878d2370eb59e74853bc63cc
carbon <carbon@milkv.io>
2023-09-08 16:38:16 +0800

    set pin 19 to GPIO in init script

--- milkv/overlay/mnt/system/duo-init.sh
+++ milkv/overlay/mnt/system/duo-init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# set PWR_GPIO[4] to gpio
+# Set Pin-29(PWR_SEQ2) to GPIO
 devmem 0x0502707c 32 0x111
 devmem 0x03001068 32 0x3
+
+# Set Pin-19(SD0_PWR_EN) to GPIO
+cvi_pinmux -w SD0_PWR_EN/XGPIOA_14 > /dev/null
 
