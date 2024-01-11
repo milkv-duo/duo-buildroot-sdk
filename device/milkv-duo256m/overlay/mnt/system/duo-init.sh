0ddc9756315ed3c35c86a273450cc09926fe346b
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2024-01-11 09:52:28 +0000

    Add zram config

--- device/milkv-duo256m/overlay/mnt/system/duo-init.sh
+++ device/milkv-duo256m/overlay/mnt/system/duo-init.sh
@@ -7,5 +7,11 @@ devmem 0x03001068 32 0x3
 # Set Pin-19(GP14) to GPIO
 duo-pinmux -w GP14/GP14 > /dev/null
 
-# insmod pwm module
+# Insmod PWM Module
 insmod /mnt/system/ko/cv181x_pwm.ko
+
+# Init Zram
+insmod /mnt/system/ko/zram.ko
+echo "128M" | tee /sys/block/zram0/disksize
+/sbin/mkswap /dev/zram0
+/sbin/swapon /dev/zram0
